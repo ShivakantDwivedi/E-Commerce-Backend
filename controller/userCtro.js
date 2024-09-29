b0c72af5d12fed4c085412b57913cadb8fe56d4f
ShivakantDwivedi <itshivakant1472002@gmail.com>
2024-09-29 23:06:42 +0530

    API update, delete, Fetch

--- controller/userCtro.js
+++ controller/userCtro.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler');
 const  User = require('../models/userModel');
 const generateToken = require('../config/jwtToken');
 
+// create a user
 const createUser = asyncHandler(async(req, res) =>{
     const email = req.body.email;
     const findUser = await User.findOne({email: email});
@@ -15,6 +16,7 @@ const createUser = asyncHandler(async(req, res) =>{
     }
 })
 
+// login a user
 const loginUserCtrl = asyncHandler(async ( req , res) => {
     const {email , password} = req.body;
 
@@ -34,5 +36,59 @@ const loginUserCtrl = asyncHandler(async ( req , res) => {
     }
 })
 
+// update a user
+const updateaUser = asyncHandler( async( req , res) => {
+    const {id} = req.params;
+    try {
+        const updatedUser  = await User.findByIdAndUpdate(id, {
+            firstname : req.body?.firstname,
+            lastname : req.body?.lastname,
+            email : req.body?.email,
+            mobile : req.body?.mobile
+        }, {
+            new : true
+        });
+        res.json(updatedUser)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
 
-module.exports = {createUser,loginUserCtrl}
+
+
+// get all user
+const getAllUsers = asyncHandler ( async (req , res) => {
+    try {
+        const getUser = await User.find()
+        res.json(getUser)
+    } catch (error) {
+            throw new Error(error)
+    }
+})
+
+
+// get a single user
+const getaUser = asyncHandler(async ( req , res) => {
+    const {id} = req.params;
+    try {
+        const  getaUser = await User.findById(id);
+        res.json(getaUser)
+    } catch (error) {
+        throw new Error(error)        
+    }
+})
+
+
+// delete a single user
+const deleteaUser = asyncHandler(async ( req , res) => {
+    const {id} = req.params;
+    try {
+        const  deleteaUser = await User.findByIdAndDelete(id);
+        res.json(deleteaUser)
+    } catch (error) {
+        throw new Error(error)        
+    }
+})
+
+
+module.exports = {createUser, loginUserCtrl, getAllUsers, getaUser, deleteaUser, updateaUser}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { createUser,loginUserCtrl } = require('../controller/userCtro');
+const { createUser, loginUserCtrl, getAllUsers,getaUser,deleteaUser, updateaUser } = require('../controller/userCtro');
 const router = express.Router();
 
 router.post('/register' , createUser);
 router.post('/login' ,loginUserCtrl)
+router.get('/all-user' , getAllUsers)
+router.get('/getauser/:id',getaUser )
+router.delete('/deleteauser/:id',deleteaUser )
+router.put("/:id" , updateaUser)
+
 
 module.exports = router
